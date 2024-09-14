1212e8e870d7554958d47dd5ce469031cfa01f0a
Eduardo Antonio Martinez <eduardoa4848@Outlook.es>
2024-09-13 22:31:43 -0500

    se crea la interfaz del proyecto y se divide las partes de la base de datos en los partials

--- App.py
+++ App.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/projects')
+def projects():
+    return render_template('views/projects/list.html')
+
+
+
 @app.route('/api/data', methods=['GET'])
 def get_data():
     data = {
@@ -22,9 +28,11 @@ def post_data():
 
 
 
+
 #connect to the database of the user 
 @app.route('/connect', methods=['POST'])
 def connect():
+    #we will get the data of the user
     host = request.form['host']
     port = request.form['port']
     database = request.form['database']
@@ -39,8 +47,39 @@ def connect():
             user=user,
             password=password
         )
+        cursor = conn.cursor()
+
+        # Obtener el listado de tablas
+        cursor.execute("""
+            SELECT table_name 
+            FROM information_schema.tables 
+            WHERE table_schema = 'public';
+        """)
+        tables = cursor.fetchall()
+
+        # Para cada tabla, obtener sus columnas y contenidos
+        tables_data = {}
+        for table in tables:
+            table_name = table[0]
+            # Obtener las columnas de la tabla
+            cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}';")
+            columns = cursor.fetchall()
+
+            # Obtener los datos de la tabla
+            cursor.execute(f"SELECT * FROM {table_name};")
+            rows = cursor.fetchall()
+
+            tables_data[table_name] = {
+                'columns': [column[0] for column in columns],
+                'rows': rows
+            }
+
+        cursor.close()
         conn.close()
-        return "Conexión exitosa!"
+
+        # Renderizar los datos en una plantilla
+        print(tables_data)
+        return render_template('index.html', tables=tables_data)
     except Exception as e:
         return f"Error al conectar: {e}"
 
